f900ffd542f809a89b250eee21ba074df81602a2
LAHAXE Arnaud <lahaxe.arnaud@gmail.com>
2023-10-16 22:17:13 +0200

    fix: remove duplicate dotenv

--- src/db/connexion.rs
+++ src/db/connexion.rs
@@ -1,11 +1,8 @@
 use diesel::prelude::*;
-use dotenv::dotenv;
 use std::env;
 
 
 pub fn establish_connection() -> SqliteConnection {
-    dotenv().ok();
-
     let database_url = env::var("DATABASE_URL")
         .expect("DATABASE_URL must be set");
 
